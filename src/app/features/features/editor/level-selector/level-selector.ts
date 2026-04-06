bd6f254e52b2799734987289bfecb0ee899c7f2b
Nabil Bennaï <bennai.nabil@gmail.com>
2026-04-06 11:16:35 +0200

    chore: project structure

---
+++ src/app/features/features/editor/level-selector/level-selector.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-level-selector',
+  imports: [],
+  templateUrl: './level-selector.html',
+  styleUrl: './level-selector.scss',
+})
+export class LevelSelector {}
---
+++ src/app/features/features/results/result-display/result-display.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-display',
+  imports: [],
+  templateUrl: './result-display.html',
+  styleUrl: './result-display.scss',
+})
+export class ResultDisplay {}
