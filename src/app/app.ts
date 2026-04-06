41d7263eb5d3e96597df64939fd609c1af5eacfe
Nabil Bennaï <bennai.nabil@gmail.com>
2026-04-06 15:57:38 +0200

    feat: integrate dynamic app title from environment configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal, afterNextRender } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs';
 import { Navbar } from './shared/components/navbar/navbar';
+import { environment } from '../environments/environment';
 
 declare const HSStaticMethods: { autoInit: () => void };
 
@@ -12,9 +13,12 @@ declare const HSStaticMethods: { autoInit: () => void };
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal(environment.appTitle);
+  protected readonly isDev = !environment.production;
 
   constructor(private router: Router) {
+    document.title = this.title();
+
     afterNextRender(() => {
       HSStaticMethods.autoInit();
     });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:3000',
+  appTitle: 'ELI5-DEV',
 };
